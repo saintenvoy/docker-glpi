e0b9bd5757312adad59460546b54847b524271d7
root <root@cicd.lan>
2024-07-23 09:23:22 -0400

    first commit

---
+++ php/scripts/mariadb-timezone.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Requires root user
+
+mariadb -h "$MARIADB_HOST" -u root -p"$MARIADB_ROOT_PASSWORD" -e "\"GRANT SELECT ON \`mysql\`.\`time_zone_name\` TO 'glpi'@'%'; FLUSH PRIVILEGES;\""
+
